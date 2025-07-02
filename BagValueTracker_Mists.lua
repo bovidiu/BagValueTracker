9940848f50da6e145211b30ddc80adc21e26eddf
Ovidiu Bokar <bovidiu87@gmail.com>
2025-07-02 22:27:30 +0100

    MoP compatibility

---
+++ BagValueTracker_Mists.lua
@@ -0,0 +1,81 @@
+-- Load the configuration file
+if not BagValueTrackerConfig then
+    BagValueTrackerConfig = {
+        enableBagValue = { true, true, true, true, true }, -- Default to enabling bag value display (disable for the first bag)
+    }
+end
+
+-- Create a frame to handle events
+local frame = CreateFrame("Frame")
+
+-- Table to hold text strings for bag value and slot overlays
+local bagSlotTexts = {}
+
+-- Function to update the slots overlay for closed bags
+local function UpdateBagSlotOverlays()
+    for bagID = 0, NUM_BAG_SLOTS do
+        local bagIcon = _G["CharacterBag" .. (bagID - 1) .. "Slot"]
+        if bagIcon then
+            local usedSlots = 0
+            local totalSlots = C_Container.GetContainerNumSlots(bagID)
+
+            if totalSlots and totalSlots > 0 then
+                for slotID = 1, totalSlots do
+                    local itemInfo = C_Container.GetContainerItemInfo(bagID, slotID)
+                    if itemInfo and itemInfo.itemID then
+                        usedSlots = usedSlots + 1
+                    end
+                end
+            end
+
+            if not bagSlotTexts[bagID] then
+                bagSlotTexts[bagID] = bagIcon:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
+                bagSlotTexts[bagID]:SetPoint("TOP", bagIcon, "TOP", 1, -17) -- Adjust vertical offset for spacing
+            end
+            local bagAvail = totalSlots - usedSlots
+            bagSlotTexts[bagID]:SetText(string.format("%d", bagAvail))
+            bagSlotTexts[bagID]:SetTextColor(1, 1, 1)
+        end
+    end
+end
+
+local function bindBagsToOpenAll() 
+     -- Initialize saved variables if not already present
+    if not BagValueTrackerDB then
+        BagValueTrackerDB = {}
+    end
+
+    -- Only bind B key on first install
+    if not BagValueTrackerDB.bindingSet then
+        SetBinding("B", "OPENALLBAGS")
+        SaveBindings(GetCurrentBindingSet()) -- Save to appropriate binding set (account or character)
+        BagValueTrackerDB.bindingSet = true
+
+        print("BagValueTracker: 'B' key has been bound to open all bags.")
+    end
+
+end
+
+-- Event handler function
+local function OnEvent(self, event, bagID)
+    if event == "BAG_OPEN" then
+        BagValue.update(bagID)
+    elseif event == "BAG_CLOSED" then
+        UpdateBagSlotOverlays()
+    elseif event == "BAG_UPDATE" then
+        BagValue.update(bagID)
+        UpdateBagSlotOverlays()
+    elseif event == "PLAYER_LOGIN" then
+        UpdateBagSlotOverlays()
+        bindBagsToOpenAll()
+    end
+end
+
+
+
+-- Register events for the frame
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("BAG_OPEN")
+frame:RegisterEvent("BAG_CLOSED")
+frame:RegisterEvent("BAG_UPDATE")
+frame:SetScript("OnEvent", OnEvent)
